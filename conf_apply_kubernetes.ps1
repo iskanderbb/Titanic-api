418847d040303201430e01dbd6313015e6dcd825
iskander <iskander.belgacem@gmail.com>
2019-12-20 03:34:07 -0500

    add ps command

--- conf_apply_kubernetes.ps1
+++ conf_apply_kubernetes.ps1
@@ -1,4 +1,3 @@
-
-
 kubectl apply -f  secret.yaml
 kubectl apply -f  mysqldb-service.yaml,mysqldb-deployment.yaml,mysqldb-claim0-persistentvolumeclaim.yaml,flask-api-deployment.yaml,flask-api-service.yaml --overwrite=true
+kubectl expose svc falsk-api --type=LoadBalancer --name=titanicapi
