1c588249cd12b95b6d79f19488e3aa115e292da2
iskander <iskander.belgacem@gmail.com>
2020-01-02 03:57:29 -0500

    ps file

--- conf_apply_kubernetes.ps1
+++ conf_apply_kubernetes.ps1
@@ -4,4 +4,4 @@ kubectl apply -f  mysqldb-service.yaml --overwrite=true
 kubectl apply -f  mysqldb-claim0-persistentvolumeclaim.yaml --overwrite=true
 kubectl apply -f  flask-api-deployment.yaml --overwrite=true
 kubectl apply -f  flask-api-service.yaml --overwrite=true
-#kubectl expose svc flask-api --type=LoadBalancer --name=titanic-api-service
+kubectl expose svc flask-api --type=LoadBalancer --name=titanic-api-service
