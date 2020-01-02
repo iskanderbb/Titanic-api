b8b1f78e4f4578dc107cde263b668fdacf866de4
iskander <iskander.belgacem@gmail.com>
2020-01-02 03:56:23 -0500

    config nodeport service

--- conf_apply_kubernetes.ps1
+++ conf_apply_kubernetes.ps1
@@ -1,3 +1,7 @@
 kubectl apply -f  secret.yaml
-kubectl apply -f  mysqldb-service.yaml,mysqldb-deployment.yaml,mysqldb-claim0-persistentvolumeclaim.yaml,flask-api-deployment.yaml,flask-api-service.yaml --overwrite=true
-kubectl expose svc flask-api --type=LoadBalancer --name=titanicapi
+kubectl apply -f  mysqldb-deployment.yaml --overwrite=true
+kubectl apply -f  mysqldb-service.yaml --overwrite=true
+kubectl apply -f  mysqldb-claim0-persistentvolumeclaim.yaml --overwrite=true
+kubectl apply -f  flask-api-deployment.yaml --overwrite=true
+kubectl apply -f  flask-api-service.yaml --overwrite=true
+#kubectl expose svc flask-api --type=LoadBalancer --name=titanic-api-service
--- swagger_server/db_config.py
+++ swagger_server/db_config.py
@@ -4,9 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-#app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'titanicdb'
 app.config['MYSQL_DATABASE_HOST'] = 'mysqldb'
-app.config['MYSQL_DATABASE_PORT'] = '3306'
 mysql.init_app(app)
